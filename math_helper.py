5af926fd629ff4e77b2f7ed06a11cf46ef081c75
Hendrik Müller <hendrik.mueller@devboost.de>
2018-03-19 23:10:48 +0100

    Rendering cube and starting to implement player controls

---
+++ math_helper.py
@@ -0,0 +1,6 @@
+class vec3:
+    def __init__(self, x: int = 0, y: int = 0, z: int = 0):
+        self.x = x
+        self.y = y
+        self.z = z
+
---
+++ player.py
@@ -0,0 +1,31 @@
+from math_helper import vec3, vec2
+from pyglet.gl import *
+import math
+
+
+class Player:
+    def __init__(self):
+        self.position = vec3()
+        self.direction = vec3(0, 0, -1)
+
+    def update(self):
+        glMatrixMode(GL_PROJECTION)
+        glTranslatef(self.position.x, self.position.y, self.position.z)
+        angle = math.atan2(self.direction.z, self.direction.x)
+        angle = (angle * 180) / math.pi
+        glRotatef(angle, 0, 1, 0)
+
+    def handle_key(self, symbol, pressed):
+        if symbol == pyglet.window.key.W and pressed:
+            self.position.z += 5
+        elif symbol == pyglet.window.key.S and pressed:
+            self.position.z -= 5
+
+        horizontal_direction = vec2(self.direction.x, self.direction.z)
+        if symbol == pyglet.window.key.E and pressed:
+            horizontal_direction.rotate(5)
+        elif symbol == pyglet.window.key.Q and pressed:
+            horizontal_direction.rotate(-5)
+        self.direction.x = horizontal_direction.x
+        self.direction.z = horizontal_direction.y
+        print(self.direction)
